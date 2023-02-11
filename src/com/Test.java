d6a5ad7bcfb5faf99cb268cf5917046b44e4cb18
Yukonoshita <101973952+Yukonoshita@users.noreply.github.com>
2023-02-11 22:35:12 +0800

    Add files via upload

---
+++ src/com/Test.java
@@ -0,0 +1,10 @@
+package com;
+
+import javax.swing.*;
+public class Test {
+   public static void main(String[] args) {
+         JFrame frame = new JFrame();
+         frame.setBounds(100, 100, 100, 100);
+         frame.setVisible(true);
+   }
+}
